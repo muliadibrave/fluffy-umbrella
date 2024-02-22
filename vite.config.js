1210d8fd3c1bafc742c5031ab568acf0aa205c9a
muliadibrave <72936896+muliadibrave@users.noreply.github.com>
2024-02-22 15:00:50 +0000

    ghost

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
+import {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-})
+  test:# https://tea.xyz/what-is-this-file
+---
+version: 1.0.0
+codeOwners:
+  - '0xF28A293A97726831E04D98E97AF826Fe8ee1FB9f'
+quorum: 1
